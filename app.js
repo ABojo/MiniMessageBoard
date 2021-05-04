4f0065c7ea7b8eac4862098d945ef58c8cafc335
ABojo <alex.bojorquez2598@gmail.com>
2021-05-04 17:26:37 -0400

    initial commit/layed out structure of application

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const messages = [
+  { text: 'This is a test message', user: 'TestUser', added: new Date() },
+  { text: 'Another test message!!', user: 'ABojo', added: new Date() },
+];
+
+app.use(express.static('public'));
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/new', (req, res) => {
+  res.send('This is the new message page');
+});
+
+app.post('/new', (req, res) => {
+  //Push new message into array
+});
+
+app.all('*', (req, res) => {
+  res.send('Sorry, that page does not exist on this server!');
+});
+
+app.listen(8080, () => {
+  console.log('Server is listenting for incoming requests!');
+});
