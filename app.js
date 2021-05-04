2a3eb38c94333caee66bbc4c2fc7b96ca8db39c4
ABojo <alex.bojorquez2598@gmail.com>
2021-05-04 17:57:21 -0400

    Finished markup/styling for home page and passed in the message array to the template

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 
 const messages = [
-  { text: 'This is a test message', user: 'TestUser', added: new Date() },
-  { text: 'Another test message!!', user: 'ABojo', added: new Date() },
+  {
+    text:
+      'This is a test message to ensure that the messages in this array are being properly interpolated into the template',
+    user: 'TestUser',
+    added: new Date(),
+  },
+  { text: 'Another test message', user: 'ABojo', added: new Date() },
 ];
 
 app.use(express.static('public'));
@@ -11,7 +16,7 @@ app.set('views', 'views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { messages: messages });
 });
 
 app.get('/new', (req, res) => {
